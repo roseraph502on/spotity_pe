fe1b90b00b1cf69174f5414964717dbd88cc1228
roseraph502on <sera6768@naver.com>
2025-06-03 03:55:49 +0900

    lazy22

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router'
@@ -10,17 +10,18 @@ import PlaylistDetailPage from './feeture/PlaylistDetailPage'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<AppLayout />}>
-        <Route index element={<Homepage />} />
-        <Route path='search' element={<SearchPage />} />
-        <Route path='search/:keyword' element={<SearchWithkeyPage />} />
-        <Route path='playlist' element={<PlaylistDetailPage />} />
-        {/* <Route path='playlist' element={<PlaylistPage/>}/> */}
-      </Route>
-      {/* <Route path='/admin' element={<AdminLayout/>}> */}
-
-    </Routes>
+    <Suspense fallback={<Loading/>}>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path='search' element={<SearchPage />} />
+          <Route path='search/:keyword' element={<SearchWithkeyPage />} />
+          <Route path='playlist' element={<PlaylistDetailPage />} />
+          <Route path='playlist' element={<PlaylistPage/>}/>
+        </Route>
+        {/* <Route path='/admin' element={<AdminLayout/>}> */}
+      </Routes>
+    </Suspense>
   );
 }
 
