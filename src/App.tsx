734986b508a8cc928d1b468713fe38f1a7cdfaab
roseraph502on <sera6768@naver.com>
2025-06-03 01:50:48 +0900

    vite >cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router'
+import AppLayout from './core/AppLayout'
+import Homepage from './feeture/Homepage'
+import SearchPage from './feeture/SearchPage'
+import SearchWithkeyPage from './feeture/SearchWithkeyPage'
+import PlaylistDetailPage from './feeture/PlaylistDetailPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path='search' element={<SearchPage />} />
+        <Route path='search/:keyword' element={<SearchWithkeyPage />} />
+        <Route path='playlist' element={<PlaylistDetailPage />} />
+        {/* <Route path='playlist' element={<PlaylistPage/>}/> */}
+      </Route>
+      {/* <Route path='/admin' element={<AdminLayout/>}> */}
+
+    </Routes>
   );
 }
 
---
+++ src/feeture/PlaylistDetailPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PlaylistDetailPage = () => {
+  return (
+    <div>
+      PlaylistDetailPage
+    </div>
+  )
+}
+
+export default PlaylistDetailPage
+
